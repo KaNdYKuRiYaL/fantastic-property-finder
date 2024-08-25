fed4735c428be14f9d398c28f1e24450e0f71b78
KaNdYKuriyal <kuriyaldivyanshu2003@gmail.com>
2024-08-25 15:40:27 +0530

    added serach for properties

--- components/PropertyCard.jsx
+++ components/PropertyCard.jsx
@@ -81,7 +81,7 @@ const PropertyCard = ({property}) => {
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="flex align-middle gap-2 mb-4 lg:mb-0">
             <FaMapMarker className="text-orange-700 mt-1"/>
-            <span className="text-orange-700">{street}{city}{state}</span>
+            <span className="text-orange-700">{street},{city},{state}</span>
           </div>
           <a
             href={`/properties/${property._id}`}
